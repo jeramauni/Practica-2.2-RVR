e044dafb7e3e7ab95f6e1f22175462503f5b2aba
Jesús Ramos Rodríguez <jeramo02@ucm.es>
2020-05-14 22:41:27 +0200

    Escritura en archivo

--- practica2.2/serializacion/ejercicio1.cc
+++ practica2.2/serializacion/ejercicio1.cc
@@ -14,25 +14,45 @@ class Jugador: public Serializable
 public:
     Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
     {
-        strncpy(name, _n, 80);
+        strncpy(name, _n, MAX_NAME);
     };
 
     virtual ~Jugador(){};
 
     void to_bin()
     {
-        //
+        int32_t total = MAX_NAME * sizeof(char) + 2 * sizeof(int16_t);
+        alloc_data(total);
+
+        char * aux = _data;
+
+        memcpy(aux, name, MAX_NAME);
+        aux += MAX_NAME * sizeof(char);
+
+        memcpy(aux, &x, sizeof(int16_t));
+        aux += sizeof(int16_t);
+
+        memcpy(aux, &y, sizeof(int16_t));
     }
 
     int from_bin(char * data)
     {
-        //
+        char * aux = data;
+
+        memcpy(name, aux, 80);
+        aux += 80 * sizeof(char);
+
+        memcpy(&x, aux, sizeof(int16_t));
+        aux += sizeof(int16_t);
+
+        memcpy(&y, aux, sizeof(int16_t));
         return 0;
     }
 
+private:
 
-public:
-    char name[80];
+    static const size_t MAX_NAME = 80;
+    char name[MAX_NAME];
 
     int16_t x;
     int16_t y;
@@ -40,12 +60,22 @@ public:
 
 int main(int argc, char **argv)
 {
-    Jugador one_r("", 0, 0);
-    Jugador one_w("Player_ONE", 123, 987);
+    Jugador one_a("Player_ONE", 123, 987);
+    Jugador one_b("Payer_TWO", 456, 654);
+
+    int file = open("data", O_CREAT|O_TRUNC);
+    if (file == -1) {
+        std::cout << "Error while opening file. \n";
+    }
+
+    one_a.to_bin();
+    
+    
+    write(file, one_a.data(), one_a.size());
+    std::cout << "Escritura finalizada.. \n";
 
     //Serializar y escribir one_w en un fichero
     //Leer el fichero en un buffer y "deserializar" en one_r
     //Mostrar el contenido one_r
-
     return 0;
 }
