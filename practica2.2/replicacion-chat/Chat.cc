fede188cc8e611d376500a3d717516dbafb0992b
Jesús Ramos Rodríguez <jeramo02@ucm.es>
2020-05-16 21:50:36 +0200

    Finalizado a la espera de ejecución

--- practica2.2/replicacion-chat/Chat.cc
+++ practica2.2/replicacion-chat/Chat.cc
@@ -4,18 +4,46 @@ void ChatMessage::to_bin()
 {
     alloc_data(MESSAGE_SIZE);
 
-    memset(_data, 0, MESSAGE_SIZE);
-
     //Serializar los campos type, nick y message en el buffer _data
+    //int32_t total = MESSAGE_SIZE + 2 * sizeof(int16_t);
+        
+    //alloc_data(total);
+
+    char * tmp = _data;
+
+    memset(tmp, 0, MESSAGE_SIZE);
+
+    memcpy(tmp, static_cast<void *>(type), sizeof(uint8_t));
+    tmp += sizeof(uint8_t);
+
+    memcpy(tmp, nick.c_str(), nick.length());
+    tmp += sizeof(char) * 8;
+
+    memcpy(tmp, message.c_str(), message.length());
 }
 
 int ChatMessage::from_bin(char * bobj)
 {
+    /*
     alloc_data(MESSAGE_SIZE);
 
-    memcpy(static_cast<void *>(_data), bobj, MESSAGE_SIZE);
+    memcpy(static_cast<void *>(_data), bobj, MESSAGE_SIZE);*/
 
     //Reconstruir la clase usando el buffer _data
+    char * tmp = bobj;
+
+    memcpy(static_cast<void *>(type), tmp, sizeof(uint8_t));
+    tmp += sizeof(uint8_t);
+
+    char[8] _nick;
+    memcpy(_nick, tmp, 8);
+    nick = convertToString(_nick, _nick.length()); 
+    tmp += 8 * sizeof(char);
+    
+    char[80] _messages;
+    memcpy(_messages, tmp, 80);
+    nick = convertToString(_messages, _messages.length()); 
+    tmp += 80 * sizeof(char);
 
     return 0;
 }
@@ -31,6 +59,67 @@ void ChatServer::do_messages()
         // - LOGIN: Añadir al vector clients
         // - LOGOUT: Eliminar del vector clients
         // - MESSAGE: Reenviar el mensaje a todos los clientes (menos el emisor)
+        Socket* client_Socket;
+        ChatMessage message_Client;
+        socket.recv(message_Client, client_Socket);
+        switch (message_Client.type)
+        {
+        case MessageType::LOGIN:
+            auto it = clients.begin();
+            while(it != clients.end() && (*it) != client_Socket)
+            {
+                it++;
+            }
+            if(it != client.end())
+            {
+                std::cout << "LOGIN FAIL: Client " << message_Client.nick <<  "is already logged\n";
+            }
+            {
+                clients.push_back(client_Socket);
+                std::cout << "New client " << message_Client.nick <<  " logged succesfully\n";
+            }
+            break;
+        case MessageType::LOGOUT:
+            auto it = clients.begin();
+            while(it != clients.end() && (*it) != client_Socket)
+            {
+                it++;
+            }
+            if(it != client.end())
+            {
+                clients.erase(it);
+                std::cout << "Client " << message_Client.nick <<  " logged out succesfully\n";
+            }
+            else
+            {
+                std::cout << "LOGOUT FAIL: Client " << message_Client.nick <<  " is not logged \n";
+            }
+            
+            break;
+        case MessageType::MESSAGE:
+            
+            auto it = clients.begin();
+            while(it != clients.end() && (*it) != client_Socket)
+            {
+                it++;
+            }
+            if(it != client.end())
+            {
+                for(auto it = clients.begin(); it != clients.end(); it++)
+                {
+                    if((*it) != client_Socket) socket.send(message_Client, *(*it));
+                }
+                std::cout << "Client " << message_Client.nick <<  " says "<< message_Client.message <<" \n"
+            }
+            else
+            {
+                std::cout << "MESSAGE FAIL: Client " << message_Client.nick <<  " is not logged \n"
+            }
+            
+            break;
+        default:
+            break;
+        }
     }
 }
 
@@ -46,15 +135,37 @@ void ChatClient::login()
 
 void ChatClient::logout()
 {
+    std::string msg;
 
+    ChatMessage em(nick, msg);
+    em.type = ChatMessage::LOGOUT;
+
+    socket.send(em, socket);
 }
 
 void ChatClient::input_thread()
 {
-    while (true)
+    bool inChat = true;
+    while (intChat)
     {
         // Leer stdin con std::getline
         // Enviar al servidor usando socket
+        std::string msg;
+
+        msg = std::getline();
+
+        if(msg == "exit" || msg == "logout")
+        {
+            logout();
+            inChat = false;
+        } 
+        else
+        {
+            ChatMessage em(nick, msg);
+            em.type = ChatMessage::MESSAGE;
+
+            socket.send(em, socket);
+        }
     }
 }
 
@@ -64,6 +175,13 @@ void ChatClient::net_thread()
     {
         //Recibir Mensajes de red
         //Mostrar en pantalla el mensaje de la forma "nick: mensaje"
+        Socket* server_Socket;
+        ChatMessage message_Server;
+        int tmp = socket.recv(message_Server, server_Socket);
+        if(tmp != -1)
+        {
+            std::cout << message_Server.nick << ": " << message_Server.message << "\n";
+        }
     }
 }
 
--- practica2.2/replicacion-chat/Socket.cc
+++ practica2.2/replicacion-chat/Socket.cc
@@ -5,9 +5,29 @@
 
 Socket::Socket(const char * address, const char * port):sd(-1)
 {
-    //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
-    //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
+    struct addrinfo hints;
+    struct addrinfo * res;
 
+    memset(&hints, 0, sizeof(struct addrinfo));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int rc = getaddrinfo(address, port, &hints, &res);
+
+    if ( rc != 0 )
+    {
+        std::cerr << "getaddrinfo: " << gai_strerror(rc) << std::endl;
+    }
+
+    // res contiene la representación como sockaddr de dirección + puerto
+
+    sd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    sa = *(res->ai_addr);
+    sa_len = res->ai_addrlen;
+    
+    freeaddrinfo(res);
 }
 
 int Socket::recv(Serializable &obj, Socket * &sock)
@@ -36,8 +56,10 @@ int Socket::recv(Serializable &obj, Socket * &sock)
 
 int Socket::send(Serializable& obj, const Socket& sock)
 {
-    //Serializar el objeto
-    //Enviar el objeto binario a sock usando el socket sd
+    obj.to_bin();
+    int bytes = sendto(sd, obj.data(), obj.size(), 0, &(sock.sa), &(sock.sa_len));
+    if(bytes <= 0) return -1;
+    return 0;
 }
 
 bool operator== (const Socket &s1, const Socket &s2)
@@ -45,6 +67,19 @@ bool operator== (const Socket &s1, const Socket &s2)
     //Comparar los campos sin_family, sin_addr.s_addr y sin_port
     //de la estructura sockaddr_in de los Sockets s1 y s2
     //Retornar false si alguno difiere
+    struct sockaddr_in * socket_f = (struct sockaddr_in *)& s1.sa;
+    struct sockaddr_in * socket_s = (struct sockaddr_in *)& s2.sa;
+    if(socket_f->sin_family == socket_s->sin_family)
+    {
+        if(socket_f->sin_addr.s_addr == socket_s->sin_addr.s_addr)
+        {
+            if(socket_f->sin_port == socket_s->sin_port)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
 };
 
 std::ostream& operator<<(std::ostream& os, const Socket& s)
