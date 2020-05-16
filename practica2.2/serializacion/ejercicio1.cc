41c82b19bf863c64823e2bc8a9425f6e72ff899d
Jesús Ramos Rodríguez <jeramo02@ucm.es>
2020-05-16 14:59:27 +0200

    Serialización finalizada

--- practica2.2/serializacion/ejercicio1.cc
+++ practica2.2/serializacion/ejercicio1.cc
@@ -9,50 +9,52 @@
 #include <string.h>
 #include <unistd.h>
 
+
 class Jugador: public Serializable
 {
 public:
     Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
     {
-        strncpy(name, _n, MAX_NAME);
+        strncpy(name, _n, 80);
     };
 
     virtual ~Jugador(){};
 
     void to_bin()
     {
-        int32_t total = MAX_NAME * sizeof(char) + 2 * sizeof(int16_t);
+        int32_t total = 80 * sizeof(char) + 2 * sizeof(int16_t);
+        
         alloc_data(total);
 
-        char * aux = _data;
+        char * tmp = _data;
 
-        memcpy(aux, name, MAX_NAME);
-        aux += MAX_NAME * sizeof(char);
+        memcpy(tmp, name, 80);
+        tmp += 80 * sizeof(char);
 
-        memcpy(aux, &x, sizeof(int16_t));
-        aux += sizeof(int16_t);
+        memcpy(tmp, &x, sizeof(int16_t));
+        tmp += sizeof(int16_t);
 
-        memcpy(aux, &y, sizeof(int16_t));
+        memcpy(tmp, &y, sizeof(int16_t));
     }
 
     int from_bin(char * data)
     {
-        char * aux = data;
+        char * tmp = data;
 
-        memcpy(name, aux, 80);
-        aux += 80 * sizeof(char);
+        memcpy(name, tmp, 80);
+        tmp += 80 * sizeof(char);
 
-        memcpy(&x, aux, sizeof(int16_t));
-        aux += sizeof(int16_t);
+        memcpy(&x, tmp, sizeof(int16_t));
+        tmp += sizeof(int16_t);
 
-        memcpy(&y, aux, sizeof(int16_t));
-        return 0;
+        memcpy(&y, tmp, sizeof(int16_t));
+
+        return sizeof(data);
     }
 
-private:
 
-    static const size_t MAX_NAME = 80;
-    char name[MAX_NAME];
+public:
+    char name[80];
 
     int16_t x;
     int16_t y;
@@ -60,22 +62,42 @@ private:
 
 int main(int argc, char **argv)
 {
-    Jugador one_a("Player_ONE", 123, 987);
-    Jugador one_b("Payer_TWO", 456, 654);
-
-    int file = open("data", O_CREAT|O_TRUNC);
-    if (file == -1) {
-        std::cout << "Error while opening file. \n";
-    }
-
-    one_a.to_bin();
-    
-    
-    write(file, one_a.data(), one_a.size());
-    std::cout << "Escritura finalizada.. \n";
+    Jugador one_r("", 0, 0);
+    Jugador one_w("Player_ONE", 123, 987);
 
     //Serializar y escribir one_w en un fichero
-    //Leer el fichero en un buffer y "deserializar" en one_r
-    //Mostrar el contenido one_r
+    int usedTxt = open("data.txt", O_CREAT|O_WRONLY, 0600);
+    if(usedTxt == -1)
+    {
+        std::cerr << "Error en la apertura \n";
+    }
+    one_w.to_bin();
+    int bytes = write(usedTxt, one_w.data(), one_w.size());
+    close(usedTxt);
+    if(bytes == -1)
+    {
+        std::cerr << "Error escritura: " << strerror(errno) << '\n';
+    }
+    else
+    {
+        //Leer el fichero en un buffer y "deserializar" en one_r
+        char buffer [one_w.size()];
+        memset(&buffer, 0 ,sizeof(buffer));
+        int readed_file = open("data.txt", O_RDONLY);
+        int bytes = read(readed_file, buffer, one_w.size());
+        close(readed_file);
+        if(bytes == -1)
+        {
+           std::cerr << "Error lectura: " << strerror(errno) << '\n'; 
+        }
+        else
+        {
+            one_r.from_bin(buffer);
+            //Mostrar el contenido one_r
+            std::cout << one_r.name << std::endl;
+            std::cout << one_r.x << std::endl;
+            std::cout << one_r.y << std::endl;
+        }    
+    }
     return 0;
 }
